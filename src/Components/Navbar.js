290136c43d92b904f337e846995da91be2323f46
prasadp <prasadp@DESKTOP-VDLHQ4T>
2025-01-24 17:08:28 +0530

    make changes in navbar crash

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Navbar</a>
+                    <Link class="navbar-brand" to="/">Navbar</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
