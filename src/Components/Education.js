58e848a74418ba85a2589f94d9a365bbf09a73f0
Prasad Pathak <prasad.pathak5555@gmail.com>
2025-02-01 09:20:59 +0530

    experience page done

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import "./Education.css";
 
 // Import the images
-import checkmark from "./assets/checkmark.png";
+import checkmark from "./assets/education.png";
 
 const Experience = () => {
     return (
@@ -15,19 +15,16 @@ const Experience = () => {
                     {/* Experience 1 */}
                     <div className="experience-info-container">
                         <div className="experience-header">
-                            <h2 className="experience-sub-title">
-                                Shaurya Technosoft Pvt. Ltd.
-                            </h2>
-                            <h3 className="experience-location">Pune, India</h3>
+                            <h2 className="experience-sub-title">Vishwabharati Academy's College Of Engineering</h2>
+                            <h3 className="experience-location">Ahmednagar, India</h3>
                         </div>
                         <div className="experience-role">
                             <h4>
-                                <img src={checkmark} alt="Experience icon" className="icon" />{" "}
-                                .NET Developer
+                                <img src={checkmark} alt="Experience icon" className="icon" />{" "}Bachlore Degree
                             </h4>
-                            <p className="experience-duration">09/2022 - Present</p>
+                            <p className="experience-duration"> 08/2014  06/2018</p>
                         </div>
-                        <p>
+                        {/* <p>
                             <strong>Company Description</strong>
                         </p>
                         <p>
@@ -35,32 +32,29 @@ const Experience = () => {
                             Manufacturing, Mining, Vehicle Tracking, Agricultural, Election
                             Management. The company delivers products such as Desktop,
                             Android Applications and Web-Based Solutions.
-                        </p>
+                        </p> */}
                     </div>
 
                     {/* Experience 2 */}
                     <div className="experience-info-container">
                         <div className="experience-header">
-                            <h2 className="experience-sub-title">
-                                ThinkQuotient Software Training Institute
-                            </h2>
-                            <h3 className="experience-location">Pune, India</h3>
+                            <h2 className="experience-sub-title">Sau.Sundarbai Manik Adsul Polytechnic</h2>
+                            <h3 className="experience-location">Ahmednagar, India</h3>
                         </div>
                         <div className="experience-role">
                             <h4>
-                                <img src={checkmark} alt="Experience icon" className="icon" />{" "}
-                                .NET Intern
+                                <img src={checkmark} alt="Experience icon" className="icon" />{" "}Diploma
                             </h4>
-                            <p className="experience-duration">05/2022 - 09/2022</p>
+                            <p className="experience-duration"> 08/2011  06/2014</p>
                         </div>
-                        <p>
+                        {/* <p>
                             <strong>Company Description</strong>
                         </p>
                         <p>
                             THINKQUOTIENT SOFTWARE PRIVATE LIMITED has been engaged in
                             research, design, Software Development & Training & Placement of
                             students who want to pursue their career in IT.
-                        </p>
+                        </p> */}
                     </div>
                 </div>
             </section>
